d5ce2410c8366e4f6de1df65dc8fb07d962b5f04
Chanakyasinde <chanakyasinde@gmail.com>
2026-03-13 10:45:00 +0530

    feat(domain): add backend/internal/domain/entities/user.go

---
+++ backend/internal/domain/entities/user.go
@@ -0,0 +1,12 @@
+package entities
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name         string             `bson:"name" json:"name"`
+	Email        string             `bson:"email" json:"email"`
+	PasswordHash string             `bson:"passwordHash" json:"-"`
+	Role         string             `bson:"role" json:"role"`
+	TimeStamp    `bson:",inline"`
+}
